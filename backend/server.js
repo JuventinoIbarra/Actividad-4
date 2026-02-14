3217504da2daa2e1fe9c500e3b72b967281b4606
Juventino Ibarra <al07133783@tecmilenio.mx>
2026-02-13 21:22:52 -0600

    Port Change

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require("./config/db.js")
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const authRoutes = require("./routes/authRoutes.js")
 const productRoutes = require("./routes/productRoutes.js")
 
